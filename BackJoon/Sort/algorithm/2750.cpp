b9d73dd1d08e51d530f81993661b0b626a3daf16
HyunIn_Asus <kimhyuninnnn@gmail.com>
2022-09-05 20:50:45 +0900

    Sort Algorithm

--- BackJoon/Sort/algorithm/2750.cpp
+++ BackJoon/Sort/algorithm/2750.cpp
@@ -4,14 +4,22 @@
 using namespace std;
 
 bool compare(int a, int b){
-    return a > b;
+    return a < b;
 }
 int main(void){
-    int a[10] = {9,3,4,5,10,7,6,2,1,8};
+    int index;
+    cin >> index;
+    int a[index] = {};
+    for(int i = 0;i < index ; i++){
+        int number;
+        cin >> number;
+        a[i] = number;
+    }
+    // int a[10] = {9,3,4,5,10,7,6,2,1,8};
     int aSize = sizeof(a)/sizeof(int);
     sort(a,a+aSize, compare);
     for(int i=0;i<aSize;i++){
-        cout << a[i] << ' ';
+        cout << a[i] << endl;
     }
     return 0;
 }
